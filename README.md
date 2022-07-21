148e8fea5454b73d17f09696b62889a560ce0d70
starkat7 <kaelbe@hotmail.com>
2022-07-20 20:51:52 -0700

    Added the wyrmWords and defined a couple starting variables

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Disgraceman
-Knights and dragons-themed hangman
+
+### Greetings, noble knight! It is time to venture forth and vanquish the ever-annoying Words Wyrm at his own game! Will you triumph or ride home in shame?
+
+(Knights and dragons-themed hangman)
---
+++ js/app.js
@@ -0,0 +1,27 @@
+console.log("Finding dragons...");
+
+//  This needs to be an array of objects that contains the secret word and its definition(to be given when the player wins)
+const wyrmwords = [
+    {word: "BUMFUZZLE", definition: "to confuse, to perplex, to fluster"},
+    {word: "CATTYWAMPUS", definition: "when something is askew or awry" },
+    {word: "TARADIDDLE", definition: "lies or pretentious nonsense"},
+    {word: "SNICKERSNEE", definition: "a large knife"},
+    {word: "WIDDERSHINS", definition: "to move in a left-handed or contrary direction"},
+    {word: "COLLYWOBBLES", definition: "a bellyache"},
+    {word: "GUBBINS", definition: "bits and pieces"},
+    {word: "BROUHAHA", definition: "a hubbub or uproar"},
+    {word: "BUMBERSHOOT", definition: "an umbrella"},
+    {word: "LOLLYGAG", definition: "to dawdle, or to fool around and waste time"},
+    {word: "FLIBBERTIGIBBET", definition: "a silly flighty person"},
+    {word: "MALARKEY", definition: "insincere or foolish talk"},
+    {word: "COMEUPPANCE", definition: "a deserved rebuke or penalty"},
+    {word: "DONNYBROOK", definition: "a free-for-all or brawl"},
+    {word: "SNOLLYGOSTER", definition: "a shrewd, unprincipled person"},
+    {word: "ERINACEOUS", definition: "like or relating to the hedgehog"},
+    {word: "BIBBLE", definition: "to drink liquor, especially continuously in small amounts"},
+    {word: "NINCOMPOOP", definition: "a foolish or silly person"}
+];
+
+//  Need to define starting variables...
+const knightsWord = "";
+let knightsChoice = "";
